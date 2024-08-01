655990fa9c7482c8b876de49c8a437097f5cea85
reyhand-ebs <mohamad.reyhand.f@students.esqbs.ac.id>
2024-08-01 07:42:56 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+
+import SearchForm from "./components/SearchForm";
+import MovieList from "./components/MovieList";
+import Navigation from "./components/Navigation";
+import LocaleContext from "./contexts/LocaleContext";
+
+import "./styles/global.css";
 
 function App() {
+  const [locale, setLocale] = useState(localStorage.getItem("locale") || "id");
+  const [movies, setMovies] = useState([]);
+
+  const toggleLocale = () => {
+    setLocale((prevLocale) => {
+      const newLocale = prevLocale === "id" ? "en" : "id";
+      localStorage.setItem("locale", newLocale);
+      return newLocale;
+    });
+  };
+
+  const selectLanguage = ({ id, en }) => {
+    if (id === undefined || en === undefined) {
+      return "language options is empty";
+    }
+    return locale === "id" ? id : en;
+  };
+
+  const localeContextValue = useMemo(() => {
+    return {
+      locale,
+      toggleLocale,
+      selectLanguage,
+    };
+  }, [locale]);
+
+  const handleSearch = (searchResults) => {
+    setMovies(searchResults);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocaleContext.Provider value={localeContextValue}>
+      <div className="movie-app">
+        <header className="movie-app__header">
+          <Navigation />
+        </header>
+        <main>
+          <SearchForm onSearch={handleSearch} />
+          <MovieList movies={movies} />
+        </main>
+      </div>
+    </LocaleContext.Provider>
   );
 }
 
